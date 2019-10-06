f2f8b551c4316914bd4ea0d05f85e4dec1e25659
Daan Verstraten <Huntondoom@gmail.com>
2019-10-06 14:33:59 +0200

    Code cleanup

--- Serialization/Classes/JSON Serializer/JSON Serializer - Variables.cs
+++ Serialization/Classes/JSON Serializer/JSON Serializer - Variables.cs
@@ -1,9 +1,11 @@
-﻿using System.Runtime.Serialization.Json;
+﻿using System;
+using System.Runtime.Serialization.Json;
 
 namespace DaanV2.Serialization {
     public partial class JSONSerializer<TypeSerialize> {
 
         /// <summary>The underlying serializer that is being connected to the interface</summary>
+        [NonSerialized]
         private DataContractJsonSerializer serializer;
     }
 }
