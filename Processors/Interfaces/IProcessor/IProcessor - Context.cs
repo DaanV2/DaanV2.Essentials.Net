97d5c476cb8fa4a00c5fc032f8e71bab5b4a0655
Daan Verstraten <Huntondoom@gmail.com>
2019-10-08 13:44:43 +0200

    Added

---
+++ Processors/Interfaces/IProcessor/IProcessor - Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaanV2.Processors {
+    ///DOLATER <summary> add description for interface: IProcessor</summary>
+    public interface IProcessor<T, TypeContext> {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Process"></param>
+        /// <param name="Context"></param>
+        void Process(T Process, TypeContext Context);
+    }
+}
---
+++ Processors/Interfaces/IProcessor/IProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaanV2.Processors {
+    ///DOLATER <summary> add description for interface: IProcessor</summary>
+    public interface IProcessor<T> {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Process"></param>
+        void Process(T Process);
+    }
+}
