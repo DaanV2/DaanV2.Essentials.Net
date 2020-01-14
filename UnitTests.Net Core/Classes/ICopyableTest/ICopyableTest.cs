407794f22b8e62612d0e0c668cf7495e94897c54
Daan Verstraten <Huntondoom@gmail.com>
2020-01-14 11:05:57 +0100

    Added Tests

---
+++ UnitTests.Net Core/Classes/ICopyableTest/ICopyableTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using DaanV2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Net_Core {
+    ///DOLATER <summary>add description for class: ICopyableTest</summary>
+    [TestClass]
+    public partial class ICopyableTest {
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void CopyString() {
+            String DataHolder = "I am so piece of text with copyable values";
+
+            String Copy = StringExtension.Copy(DataHolder);
+
+            if (Copy.Length != DataHolder.Length) {
+                Assert.Fail("Copy is not the same length as original");
+            }
+
+            for (Int32 I = 0; I < DataHolder.Length; I++) {
+                if (DataHolder[I] != Copy[I]) {
+                    Assert.Fail("Source and copy are not the same");
+                }
+            }
+
+            if (Object.ReferenceEquals(DataHolder, Copy)) {
+                Assert.Fail("Source and copy are the same object");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void CopyStringArrayTest() {
+            String[] Values = new String[] { "Something to test", "", "I am a longer piece of text" };
+            String[] Copy = Values.Copy();
+
+            if (Copy.Length != Values.Length) {
+                Assert.Fail("Copy is not the same length as original");
+            }
+
+            String V, C;
+
+            for (Int32 I = 0; I < Values.Length; I++) {
+                V = Values[I];
+                C = Copy[I];
+
+                if (C.Length != V.Length) {
+                    Assert.Fail("Copy is not the same length as original");
+                }
+
+                for (Int32 J = 0; J < V.Length; J++) {
+                    if (V[J] != C[J]) {
+                        Assert.Fail("Source and copy are not the same");
+                    }
+                }
+
+                if (!String.IsNullOrEmpty(V)) {
+                    if (Object.ReferenceEquals(V, C)) {
+                        Assert.Fail("Source and copy are the same object");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void CopyStringListTest() {
+            List<String> Values = new List<String> { "Something to test", "", "I am a longer piece of text" };
+            List<String> Copy = Values.Copy();
+
+            if (Copy.Count != Values.Count) {
+                Assert.Fail("Copy is not the same length as original");
+            }
+
+            String V, C;
+
+            for (Int32 I = 0; I < Values.Count; I++) {
+                V = Values[I];
+                C = Copy[I];
+
+                if (C.Length != V.Length) {
+                    Assert.Fail("Copy is not the same length as original");
+                }
+
+                for (Int32 J = 0; J < V.Length; J++) {
+                    if (V[J] != C[J]) {
+                        Assert.Fail("Source and copy are not the same");
+                    }
+                }
+
+                if (!String.IsNullOrEmpty(V)) {
+                    if (Object.ReferenceEquals(V, C)) {
+                        Assert.Fail("Source and copy are the same object");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UnitTests.Net Core/Classes/SerializationTests/SerializationTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using DaanV2.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Net_Core {
+    ///DOLATER <summary>add description for class: SerializationTests</summary>
+    [TestClass]
+    public partial class SerializationTests {
+        [TestMethod]
+        public void SerializationLoadTest() {
+            List<String> Items = Serialization.GetFactoryNames();
+
+            Boolean Json = false, Xml = false;
+
+            foreach (String Item in Items) {
+                if (Item == "json") {
+                    Json = true;
+                }
+
+                if (Item == "xml") {
+                    Xml = true;
+                }
+            }
+
+            Assert.IsFalse(Json, "Json factory not found");
+            Assert.IsFalse(Xml, "Xml factory not found");
+        }
+    }
+}
