df1dc28f7c7561b3e6349d97ec84b45dd1cd907e
Daan Verstraten <Huntondoom@gmail.com>
2020-01-18 13:41:57 +0100

    Updated

---
+++ UnitTests.Net Core/Classes/IReplaceable/IReplaceable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using DaanV2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Net_Core {
+    ///DOLATER <summary>add description for class: IReplaceable</summary>
+    [TestClass]
+    public partial class IReplaceable {
+        [TestMethod]
+        public void ListString() {
+            List<String> Values = new List<String> { "Example1", "Example2", "Example3", "Example4" };
+
+            this.TestList(Values, "Example", "NewExample");
+            this.TestList(Values, "Something", "AbsolutlyNothing");
+        }
+
+        private void TestList(List<String> Values, String Old, String New) {
+            List<String> Temp = Values.Copy();
+            Temp.Replace(Old, New);
+
+            if (Values.Count != Temp.Count) {
+                Assert.Fail("Count amount seems to change");
+            }
+
+            for (Int32 I = 0; I < Values.Count; I++) {
+                if (Values[I].Replace(Old, New) != Temp[I]) {
+                    Assert.Fail("Replacement failed");
+                }
+            }
+        }
+    }
+}
