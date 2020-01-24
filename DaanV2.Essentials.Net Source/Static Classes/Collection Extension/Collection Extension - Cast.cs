edeccea112f671a15fb0f1fb603d9d34144feb62
Daan Verstraten <Huntondoom@gmail.com>
2020-01-24 10:51:33 +0100

    Updated

---
+++ DaanV2.Essentials.Net Source/Static Classes/Collection Extension/Collection Extension - Cast.cs
@@ -0,0 +1,56 @@
+﻿/*ISC License
+
+Copyright(c) 2019, Daan Verstraten, daanverstraten@hotmail.com
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.*/
+using System;
+using System.Collections.Generic;
+
+namespace DaanV2 {
+    public static partial class CollectionExtension {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Values"></param>
+        /// <returns></returns>
+        public static List<T> ToList<T>(T[] Values) {
+            Int32 Count = Values.Length;
+            List<T> Out = new List<T>(Count);
+
+            for (Int32 I = 0; I < Count; I++) {
+                Out.Add(Values[I]);
+            }
+
+            return Out;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Values"></param>
+        /// <returns></returns>
+        public static List<T> ToList<T>(IList<T> Values) {
+            Int32 Count = Values.Count;
+            List<T> Out = new List<T>(Count);
+
+            for (Int32 I = 0; I < Count; I++) {
+                Out.Add(Values[I]);
+            }
+
+            return Out;
+        }
+    }
+}
