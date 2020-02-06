83769b13f42947ee9b19ca840e91f8c03f8094d9
Daan Verstraten <Huntondoom@gmail.com>
2020-02-06 10:50:16 +0100

    Updated Code

--- DaanV2.Essentials.Net Source/Enumerators/Path Type/Path Type.cs
+++ DaanV2.Essentials.Net Source/Enumerators/Path Type/Path Type.cs
@@ -21,7 +21,6 @@ namespace DaanV2 {
     /// <summary>A enumerator that helps indentifing what kind of path has been filled in</summary>
 	[Serializable, DataContract]
     public enum PathType : Byte {
-
         ///<summary>Marks that the given path belongs to a file</summary>
         File,
         /// <summary>Marks that the given path is a folder</summary>
