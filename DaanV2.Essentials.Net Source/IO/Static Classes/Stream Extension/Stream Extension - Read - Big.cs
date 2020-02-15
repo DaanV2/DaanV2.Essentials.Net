832c1eeae74df71e281e731eb6d06b6e0ea18069
Daan Verstraten <Huntondoom@gmail.com>
2020-02-15 17:54:08 +0100

    Updated

---
+++ DaanV2.Essentials.Net Source/IO/Static Classes/Stream Extension/Stream Extension - Read - Big.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaanV2.IO {
+    ///DOLATER <summary>add description for class: StreamExtension</summary>
+    public static partial class StreamExtension {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static Int32 ReadBigEndian(this Stream stream) {
+
+        }
+    }
+}
