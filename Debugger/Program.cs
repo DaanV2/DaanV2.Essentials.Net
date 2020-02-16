9e80a3080674d4d0c663640e3277d5e535d0f567
Daan Verstraten <Huntondoom@gmail.com>
2020-02-17 00:05:40 +0100

    Restructure

--- Debugger/Program.cs
+++ Debugger/Program.cs
@@ -1,25 +1,60 @@
 ﻿using System;
-using System.IO;
+using System.Diagnostics;
 
 namespace Debugger {
     internal class Program {
         private static void Main(String[] args) {
-            MemoryStream stream = new MemoryStream(new Byte[] { 0xAA, 0xBB, 0xCC, 0xDD });
+            Byte[] Data = new Byte[] { 0xAA, 0xBB, 0xCC, 0xDD, 0xAA, 0xBB, 0xCC, 0xDD };
+            Stopwatch sw = new Stopwatch();
 
-            Int32 Out = 0;
-            Int32 Length = sizeof(Int32);
-            Byte[] Data = new Byte[Length];
-
-            stream.Read(Data, 0, Length);
             unsafe {
-                Byte* p = (Byte*)&Out;
+                for (Int32 I = 0; I < 10; I++) {
 
-                *p = Data[0];
+                    sw.Start();
+                    for (Int32 J = 0; J < 10000000; J++) {
+                        Int64 Out;
+#if UNSAFE
+                        unsafe {
+                            Byte* d = (Byte*)&Out;
 
-                for (Int32 I = 1; I < Length; I++) {
-                    *(p + I) = Data[I];
+                            *d = Data[7];
+                            *(d + 1) = Data[6];
+                            *(d + 2) = Data[5];
+                            *(d + 3) = Data[4];
+                            *(d + 4) = Data[3];
+                            *(d + 5) = Data[2];
+                            *(d + 6) = Data[1];
+                            *(d + 7) = Data[0];
+                        }
+#else
+                        Out = Data[7];
+                        Out |= ((Int64)Data[6] << 8);
+                        Out |= ((Int64)Data[5] << 16);
+                        Out |= ((Int64)Data[4] << 24);
+                        Out |= ((Int64)Data[3] << 32);
+                        Out |= ((Int64)Data[2] << 40);
+                        Out |= ((Int64)Data[1] << 48);
+                        Out |= ((Int64)Data[0] << 56);
+#endif
+                    }
                 }
+                sw.Stop();
             }
+
+            Print(sw, 10 * 10000000);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sw"></param>
+        /// <param name=""></param>
+        public static void Print(Stopwatch sw, Double ItemCount = 1) {
+            Console.WriteLine("|Ms|Ticks|");
+            Console.WriteLine("|---|---|");
+            Console.WriteLine("|{0}|{1}|", sw.ElapsedMilliseconds, sw.ElapsedTicks);
+            Console.WriteLine("|{0}|{1}|", sw.ElapsedMilliseconds / ItemCount, sw.ElapsedTicks / ItemCount);
         }
     }
 }
---
+++ UnitTests/UnitTests.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests.Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests.Net")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c5d48d0b-af03-4fb6-a360-67da5856b1be")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
