4d4a9b834ef56eaa3dda8eda8c068ee394b32a4b
Daan Verstraten <Huntondoom@gmail.com>
2020-02-17 14:39:52 +0100

    Updated

--- Debugger/Program.cs
+++ Debugger/Program.cs
@@ -4,44 +4,8 @@ using System.Diagnostics;
 namespace Debugger {
     internal class Program {
         private static void Main(String[] args) {
-            Byte[] Data = new Byte[] { 0xAA, 0xBB, 0xCC, 0xDD, 0xAA, 0xBB, 0xCC, 0xDD };
-            Stopwatch sw = new Stopwatch();
+            
 
-            unsafe {
-                for (Int32 I = 0; I < 10; I++) {
-
-                    sw.Start();
-                    for (Int32 J = 0; J < 10000000; J++) {
-                        Int64 Out;
-#if UNSAFE
-                        unsafe {
-                            Byte* d = (Byte*)&Out;
-
-                            *d = Data[7];
-                            *(d + 1) = Data[6];
-                            *(d + 2) = Data[5];
-                            *(d + 3) = Data[4];
-                            *(d + 4) = Data[3];
-                            *(d + 5) = Data[2];
-                            *(d + 6) = Data[1];
-                            *(d + 7) = Data[0];
-                        }
-#else
-                        Out = Data[7];
-                        Out |= ((Int64)Data[6] << 8);
-                        Out |= ((Int64)Data[5] << 16);
-                        Out |= ((Int64)Data[4] << 24);
-                        Out |= ((Int64)Data[3] << 32);
-                        Out |= ((Int64)Data[2] << 40);
-                        Out |= ((Int64)Data[1] << 48);
-                        Out |= ((Int64)Data[0] << 56);
-#endif
-                    }
-                }
-                sw.Stop();
-            }
-
-            Print(sw, 10 * 10000000);
             Console.ReadLine();
         }
 
---
+++ UnitTests/UnitTests.Net Unsafe/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests.Net Unsafe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests.Net Unsafe")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("656528fe-7361-4036-82a0-af32155d72d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
