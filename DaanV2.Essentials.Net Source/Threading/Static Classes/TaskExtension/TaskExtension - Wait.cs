24efabde937d32bdf6980db7c18a320e87b1cf1d
DaanV2 <daanverstraten@hotmail.com>
2020-12-27 14:52:26 +0100

    Added task extension, to also work on FuncResult<U>

---
+++ DaanV2.Essentials.Net Source/Threading/Static Classes/TaskExtension/TaskExtension - Wait.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+namespace DaanV2.Threading {
+    ///DOLATER <summary>add description for class: TaskExtension</summary>
+    public static partial class TaskExtension {
+        /// <summary>Waits for all of the provided <see cref="Task"/> objects to complete execution.</summary>
+        /// <param name="Tasks">An array of <see cref="Task"/> instances on which to wait.</param>
+        public static void WaitAll(this Task[] Tasks) {
+            Task.WaitAll(Tasks);
+        }
+
+        /// <summary>Waits for all of the provided <see cref="FuncResult{T}"/> tasks objects to complete execution.</summary>
+        /// <param name="Result"></param>
+        public static void WaitAll<T>(this FuncResult<T> Result) {
+            Task.WaitAll(Result.Tasks);
+        }
+
+        /// <summary>Waits for any of the provided System.Threading.Tasks.Task objects to complete execution.</summary>
+        /// <param name="Tasks">An array of <see cref="Task"/> instances on which to wait.</param>
+        public static void WaitAny(this Task[] Tasks) {
+            Task.WaitAny(Tasks);
+        }
+
+        /// <summary>Waits for any of the provided <see cref="FuncResult{T}"/> tasks objects to complete execution.</summary>
+        /// <param name="Result"></param>
+        public static void WaitAny<T>(this FuncResult<T> Result) {
+            Task.WaitAny(Result.Tasks);
+        }
+    }
+}
