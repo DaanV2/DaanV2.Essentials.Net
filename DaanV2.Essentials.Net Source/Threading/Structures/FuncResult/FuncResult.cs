3af879c6e689af619bb360f79c29db5218e39c9a
DaanV2 <daanverstraten@hotmail.com>
2020-12-27 14:52:07 +0100

    Added Comments

--- DaanV2.Essentials.Net Source/Threading/Structures/FuncResult/FuncResult.cs
+++ DaanV2.Essentials.Net Source/Threading/Structures/FuncResult/FuncResult.cs
@@ -1,9 +1,10 @@
 ﻿using System.Threading.Tasks;
 
 namespace DaanV2.Threading {
-    ///DOLATER <summary>add description for struct: FuncResult</summary>
+    /// <summary>The struct that represents the output of a parralel <see cref="System.Func{T, TResult}"/> process</summary>
+    /// <typeparam name="U">The list of items to be returned</typeparam>
     public readonly partial struct FuncResult<U> {
-        /// <summary>Creates a new instance of <see cref="FuncResult"/></summary>
+        /// <summary>Creates a new instance of <see cref="FuncResult{U}"/></summary>
         /// <param name="tasks"></param>
         /// <param name="output"></param>
         public FuncResult(Task[] tasks, U[] output) {
@@ -11,7 +12,10 @@ namespace DaanV2.Threading {
             this.Output = output;
         }
 
+        /// <summary>The array of <see cref="Task"/> that is returned so that the programmer can wait for this tasks to be completed</summary>
         public readonly Task[] Tasks;
+
+        /// <summary>The array of output items to be returned, must be the same size as the inputted collection</summary>
         public readonly U[] Output;        
     }
 }
