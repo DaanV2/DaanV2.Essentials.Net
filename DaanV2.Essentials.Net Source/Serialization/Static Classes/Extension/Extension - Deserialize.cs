9fc432b147ffc8e53c3366d681544cca4f324b76
DaanV2 <daanverstraten@hotmail.com>
2021-01-19 12:22:49 +0100

    Added serialize extensions code for interfaces

---
+++ DaanV2.Essentials.Net Source/Serialization/Static Classes/Extension/Extension - Deserialize.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace DaanV2.Serialization {
+    public static partial class Extension {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TypeOut"></typeparam>
+        /// <typeparam name="TypeStream"></typeparam>
+        /// <param name="Serializer"></param>
+        /// <param name="Filepath"></param>
+        /// <returns></returns>
+        public static TypeOut Deserialize<TypeOut, TypeStream>(this IDeserializer<TypeOut, TypeStream> Serializer, String Filepath)
+            where TypeStream : Stream {
+
+            Stream stream = new FileStream(Filepath, FileMode.Open, FileAccess.ReadWrite);
+            TypeOut Out = Serializer.Deserialize((TypeStream)stream);
+            stream.Close();
+
+            return Out;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TypeOut"></typeparam>
+        /// <typeparam name="TypeStream"></typeparam>
+        /// <typeparam name="TypeContext"></typeparam>
+        /// <param name="Serializer"></param>
+        /// <param name="Context"></param>
+        /// <param name="Filepath"></param>
+        /// <returns></returns>
+        public static TypeOut Deserialize<TypeOut, TypeStream, TypeContext>(this IDeserializer<TypeOut, TypeStream, TypeContext> Serializer, TypeContext Context, String Filepath)
+            where TypeStream : Stream {
+
+            Stream stream = new FileStream(Filepath, FileMode.Open, FileAccess.ReadWrite);
+            TypeOut Out = Serializer.Deserialize((TypeStream)stream, Context);
+            stream.Close();
+
+            return Out;
+        }
+    }
+}
---
+++ DaanV2.Essentials.Net Source/Serialization/Static Classes/Extension/Extension - Serialize.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace DaanV2.Serialization {
+
+    public static partial class Extension {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TypeIn"></typeparam>
+        /// <typeparam name="TypeStream"></typeparam>
+        /// <param name="Serializer"></param>
+        public static void Serialize<TypeIn, TypeStream>(this ISerializer<TypeIn, TypeStream> Serializer, TypeIn O, String Filepath)
+            where TypeStream : Stream {
+
+            Stream stream = new FileStream(Filepath, FileMode.Create, FileAccess.ReadWrite);
+            Serializer.Serialize(O, (TypeStream)stream);
+            stream.Flush();
+            stream.Close();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TypeIn"></typeparam>
+        /// <typeparam name="TypeStream"></typeparam>
+        /// <param name="Serializer"></param>
+        public static void Serialize<TypeIn, TypeStream, TypeContext>(this ISerializer<TypeIn, TypeStream, TypeContext> Serializer, TypeIn O, TypeContext Context, String Filepath)
+            where TypeStream : Stream {
+
+            Stream stream = new FileStream(Filepath, FileMode.Create, FileAccess.ReadWrite);
+            Serializer.Serialize(O, (TypeStream)stream, Context);
+            stream.Flush();
+            stream.Close();
+        }
+    }
+}
