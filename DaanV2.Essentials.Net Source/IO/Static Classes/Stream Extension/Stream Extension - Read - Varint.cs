31455f2efc709fef977484be31514e23b3a0c6be
DaanV2 <daanverstraten@hotmail.com>
2021-08-04 15:31:51 +0200

    Updated

--- DaanV2.Essentials.Net Source/IO/Static Classes/Stream Extension/Stream Extension - Read - Varint.cs
+++ DaanV2.Essentials.Net Source/IO/Static Classes/Stream Extension/Stream Extension - Read - Varint.cs
@@ -5,16 +5,22 @@ namespace DaanV2.IO {
     public static partial class StreamExtension {
         /// <summary>Read an varint from stream into a <see cref="Int32" /></summary>
         /// <param name="stream">The stream to read from</param>
+        /// <param name="Count">The amount of bytes read</param>
         /// <returns>Read an varint from stream into a <see cref="Int32" /></returns>
-        public static Int32 Varint_ReadInt32(this Stream stream) {
-            return Binary.BitConverter.Varint.ReadInt32(stream, out Int32 Count);
+        public static Int32 Varint_ReadInt32(this Stream stream, out Int32 Count) {
+            Int32 Out = Binary.BitConverter.Varint.ReadInt32(stream, out Int32 bCount);
+            Count = bCount;
+            return Out;
         }
 
         /// <summary>Read an varint from stream into a <see cref="Int64" /></summary>
         /// <param name="stream">The stream to read from</param>
+        /// <param name="Count">The amount of bytes read</param>
         /// <returns>Read an varint from stream into a <see cref="Int64" /></returns>
-        public static Int64 Varint_ReadInt64(this Stream stream) {
-            return Binary.BitConverter.Varint.ReadInt64(stream, out Int32 Count);
+        public static Int64 Varint_ReadInt64(this Stream stream, out Int32 Count) {
+            Int64 Out = Binary.BitConverter.Varint.ReadInt64(stream, out Int32 bCount);
+            Count = bCount;
+            return Out;
         }
     }
 }
