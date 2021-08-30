c01713fe3896b0a2ea8779f88ea369f0bb635de1
DaanV2 <daanverstraten@hotmail.com>
2021-08-31 00:22:29 +0200

    Cleaned

--- DaanV2.Essentials.Net/IO/Classes/LittleEndianWriter/LittleEndianWriter - Function.cs
+++ DaanV2.Essentials.Net/IO/Classes/LittleEndianWriter/LittleEndianWriter - Function.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DaanV2.IO {
     public partial class LittleEndianWriter : IEndianWriter {
